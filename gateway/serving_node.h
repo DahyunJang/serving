5cc8830ba7eef97b679fb488eb9e65fa4558493b
dahyun_jang <dahyun_jang@tmax.co.kr>
2018-05-24 17:15:15 +0900

    serving_node 내용 추가 작성

--- gateway/serving_node.h
+++ gateway/serving_node.h
@@ -1,12 +1,15 @@
 #ifndef SERVING_GATEWAY_SERVING_NODE_H_
 #define SERVING_GATEWAY_SERVING_NODE_H_
 
+#include <string>
+#include <vector>
 
 #include "grpc++/create_channel.h"
 #include "grpc++/security/credentials.h"
 #include "tensorflow/core/lib/core/errors.h"
 #include "tensorflow/core/lib/core/status.h"
-
+#include "tensorflow/core/paltform/mutex.h"
+#include "tensorflow/core/paltform/thread_annotations.h"
 
 
 using grpc::Channel;
@@ -20,72 +23,106 @@ using tensorflow::serving::PredictionService;
 namespace tensorflow {
 namesapce serving{
 
+class ModelId{
+public:
+    ModelId(string& name, int64 version):
+    name_(name), version_(version){}
+
+    ModelId(const ModelSpec& model_spec):
+    name_(model_spec.name()), version_(model_spec.version().value()){}
+
+
+    /* vector find 에서 사용되는거 맞지? */
+    bool operator==(const ModelId& rhs){
+        return ((this.name_ == rhs.name_)
+                && (this.version_ == rhs.version_));
+    }
+
+private:
+    const string name_;            /* model_name */
+    const int64 version_;          /* model_version */
+};
+
+
 
 /// ServingNode holds prediction service interface
 /// it caches loaded models on serving_node
-
 class ServingNode {
 public:
-    /* how to handle stub error ?? */
+
+    /* 중간에 뻑나면 어떻게 처리하지? */
     /* server_port is [server ip]:[server] */
     ServingNode(const string server_port)
-        :server_port_(server_port)
-        //TODO initialize handling_model_status_;
-    {
-        //channel type?
-        channel = grpc::CreateChannel(server_port,
-                                      grpc::InsecureChannelCredentials());
+        :server_port_(server_port){
+        stub_(PredictionService::NewStub(
+                  grpc::CreateChannel(server_port,
+                                      grpc::InsecureChannelCredentials())));
+    }
 
-        // chan
-        stub_(PredictionService::NewStub(channel));
+    bool operator==(const ServingNode& rhs){
+        return (this.server_port_ == rhs.server_port_)
     }
 
     Status FilePredict(const FilePredictRequest &request,
                        FilePredictResponse *response){
-        ModelSpec model_spec = request.; ///TODO reference?
+        const ModelSpec& model_spec = request.model_spec();
 
         if (IsServingModel(model_spec)){
             return  FilePredict_(request, response);
         }
         else{
-            return status_failed;      /* TODO failed */
+            return tensorflow::errors::NotFound("Could not find the version ",
+                                                model_spec.version().value(),
+                                                "of model ",
+                                                model_spec.name());
         }
-
     }
 
-    Status RestoreLoadedModels(){
-        // get loaded models info
-
+    // get loaded models info
+    Status RestoreModelIds(){
         Status status;
-        // build request
 
-        status = GetModelStatus_(request, response);
-        for (){
-            //newly list up models and replace old one.
+        GetModelStatusRequest request;
+        GetModelStatusResponse response;
+
+        status = GetModelStatus_(request, &response);
+        if () {
+            for (){
+                ModelVersionStatus* version_status
+                    = response.get_model_version_status();// TODO 함수이름뭐져 =_=?
+                //newly list up models and replace old one.
+            }
+
         }
     }
-    Status FlushModels(){
-        // flush all loaded models
-    }
+
     Status LoadModel(ModelSpec& model_spec){
-        if (status = IsServingModel(model_spec)) {
-            return ForwardHandleReload
+        if (!IsServingModel(model_spec)){
+            Status status;
+
+            ReloadConfigRequest request;
+            ReloadConfigResponse response;
+            //TODO build request
+            status = HandleReloadConfigRequest_(request, &response);
+            //TODO response 처리 ?
         }
         else { // already servable
-            return // okay status.
+            return Status::OK();
         }
-
     }
     Status UnloadModel(ModelSpec& model_spec){
+        if (IsServingModel(model_spec)){
+            Status status;
 
-    }
-
-    bool IsServingModel(ModelSpec& model_spec){
-        // check model spec is in hanlding_model_status_ and its loading status
-    }
-
-    Status DumpServingModels(){
-
+            ReloadConfigRequest request;
+            ReloadConfigResponse response;
+            //TODO build request
+            status = HandleReloadConfigRequest_(request, &response);
+            //TODO response 처리 ?
+        }
+        else {
+            return Status::OK();
+        }
     }
 
 private:
@@ -112,12 +149,34 @@ private:
         return stub_->HandleReloadConfigRequest(&context, reqeust, response);
     }
 
-    //This is cache of model stauts in serving node.
-    //cache is updated at it loaded/unloaded or unexpected failed.
-    std::unique_ptr<vector> model_status_;
-    void UpdateModelStatus_(ModelSpec& model_spec, Status status){
 
+    void AddModelId(const ModelSpec& model_spec)
+        LOCKS_EXCLUDED(mu_)
+    {
+        mutex_lock l(mu_);
+        ModelId model_id(model_spec);
+        model_ids_.push_back(std::move(model_id));
     }
+
+    void RemoveModelId(const ModelSpec& model_spec)
+        LOCKS_EXCLUDED(mu_)
+    {
+        mutex_lock l(mu_);
+        ModelId model_id(model_spec);
+        model_ids_.remove(model_ids_.begin(), model_ids_.end(), model_id);
+    }
+
+    bool IsServingModel(ModelSpec& model_spec)
+        LOCKS_EXCLUDED(mu_)
+    {
+        mutex_lock l(mu_);
+        ModelId model_id(model_spec);
+        model_ids_.find(model_ids_.begin(), model_ids_.end(), model_id);
+    }
+
+    //This is kind a cache of model stauts in serving node.
+    mutex mu_;
+    std::vector<ModelId> model_ids_ GUARDED_BY(mu_);
 }
 
 
--- gateway/serving_node_pool.h
+++ gateway/serving_node_pool.h
@@ -3,10 +3,14 @@
 
 #include "tensorflow/core/lib/core/errors.h"
 #include "tensorflow/core/lib/core/status.h"
+#include "tensorflow/core/paltform/mutex.h"
+#include "tensorflow/core/paltform/thread_annotations.h"
+
 
 #include "gateway/serving_node.h"
 
 
+//흠 거의 단일 스레드 워크인데 뮤텍스가 필요할까?
 namespace tensorflow {
 namesapce serving{
 
@@ -17,28 +21,34 @@ namesapce serving{
 */
 class ServingNodePool {
 public:
-    //vector element 는 자동 destruct 되나 ??
-
-
-    Status AddServingNode(ServingNode &serving_node){
-        // Does ServingNode need to implement equal comparator ?
-
+    //TODO serving_nodes_에서 shared ptr 타입으로 바꾸고 싶은데..
+    Status AddServingNode(ServingNode &serving_node)
+        LOCKS_EXCLUDED(mu_)
+    {
+        mutex_lock l(mu_);
+        serving_nodes_.push_back(serving_node);
     }
-    Status RemoveServingNode(ServingNode &serving_node){
-
+    Status RemoveServingNode(ServingNode &serving_node)
+        LOCKS_EXCLUDED(mu_)
+    {
+        mutex_lock l(mu_);
+        serving_nodes_.remove(serving_nodes_.begin(), serving_nodes_.end(),
+            serving_node);
     }
 
+    //TODO position
     ServingNode GetServingNodeCandidate(){
         //returns
-        //currently, has no policy though.
+        //currently, it has no proper policy.
 
     }
 
 private:
     /// TODO
     //MonitorServingNode();
+    mutex mu_;
+    std::vector<ServingNode> serving_nodes_ GUARDED_BY(mu_);
 
-    std::vector<ServingNode> serving_nodes_;
 }
 
 } //namespace tensorflow
